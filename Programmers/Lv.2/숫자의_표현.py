a8fc20b41e6f6a2013888b74ba03f884f70ad0c5
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-06-03 16:13:22 +0900

    Create 숫자의_표현.py

--- Programmers/Lv.2/숫자의_표현.py
+++ Programmers/Lv.2/숫자의_표현.py
@@ -1,11 +1,12 @@
 def solution(n):
-    answer = 1
-    for i in range(1, round(n/2)+1):
-        temp = 0
-        for j in range(i, n):
-            temp+=j
-            if temp>=n:
+    answer = 0
+    for i in range(1, n+1):
+        sum = 0
+        for j in range(i, n+1):
+            sum += j
+            if sum == n:
+                answer += 1
+            elif sum > n:
                 break
-        if temp==n:
-            answer+=1
+                
     return answer
