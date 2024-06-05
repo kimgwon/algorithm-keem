ec5ba53337937a9914b9982ff399d3fabf7b985f
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-06-05 16:11:27 +0900

    Create 이진_변환_반복하기.py

--- Programmers/Lv.2/이진_변환_반복하기.py
+++ Programmers/Lv.2/이진_변환_반복하기.py
@@ -1,17 +1,12 @@
-def toTwo(x):
-    answer = ''
-    while x!=0:
-        answer+=str(x%2)
-        x//=2
-    return answer
-
 def solution(s):
-    result1, result2 = 0, 0
+    cnt = 0
+    sum_rm_0 = 0
     
-    while s!='1':
-        new_s = ''.join([i for i in s if i=='1'])
-        result1 += len(s)-len(new_s)
-        s = toTwo(len(new_s))
-        result2 +=1
+    while s != '1' :
+        cnt += 1
+        rm_0 = s.count('0')
+        sum_rm_0 += rm_0
+        s = bin(len(s) - rm_0)[2:]
+        print(bin(len(s) - rm_0))
         
-    return [result2, result1]
+    return [cnt, sum_rm_0]
