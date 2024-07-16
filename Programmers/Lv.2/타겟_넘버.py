73c2952119a4002ede41498a044fda8eaffba0fa
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-07-16 10:31:02 +0900

    Creat 타겟_넘버.py

--- Programmers/Lv.2/타겟_넘버.py
+++ Programmers/Lv.2/타겟_넘버.py
@@ -10,3 +10,19 @@ def solution(numbers, target):
                 answer += 1
     
     return answer
+
+# DFS 풀이
+# def solution(numbers, target):
+#     if not numbers and target == 0 :
+#         return 1
+#     elif not numbers:
+#         return 0
+#     else:
+#         return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+
+# product 풀이
+# from itertools import product
+# def solution(numbers, target):
+#     l = [(x, -x) for x in numbers]
+#     s = list(map(sum, product(*l)))
+#     return s.count(target)
