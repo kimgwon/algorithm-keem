7fa19f8af35275865a5096aad8e6ef04f71212ae
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-08-15 20:21:01 +0900

    Create 하노이_탑.py

---
+++ Programmers/Lv.2/하노이_탑.py
@@ -0,0 +1,8 @@
+def solution(n):
+    def hanoi(start, target, temp, n):
+        if n == 1:
+            return [[start, target]]
+        
+        return hanoi(start, temp, target, n-1) + [[start, target]] + hanoi(temp, target, start, n-1)
+        
+    return hanoi(1, 3, 2, n)
