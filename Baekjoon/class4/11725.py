dc9f9f789a1a5dc1fd714230eb433fd8236dbe21
kimgwon <92065911+kimgwon@users.noreply.github.com>
2025-01-08 11:15:40 +0900

    Create 11725.py

--- Baekjoon/class4/11725.py
+++ Baekjoon/class4/11725.py
@@ -1,15 +1,25 @@
 import sys
-from collections import defaultdict
+from collections import deque
 input = sys.stdin.readline
 
 N = int(input().strip())
-tree = defaultdict(set)
-result = [0 for _ in range(N)]
+tree = [[] for _ in range(N+1)]
+parent = [0 for _ in range(N+1)]
+parent[0] = 1
+parent[1] = 1
+queue = deque([1])
 
 for _ in range(N - 1):
     i, j = map(int, input().split())
-    tree[i].add(j)
-    tree[j].add(i)
+    tree[i].append(j)
+    tree[j].append(i)
 
-result[0] = 1
-stack = []
+while queue:
+    node = queue.popleft()
+    for i in tree[node]:
+        if parent[i] == 0:
+            parent[i] = node
+            queue.append(i)
+
+for i in range(2, N+1):
+    print(parent[i])
