31d9d22e4b5e2cf4b6ac088c4628a9fbe4cd3393
kimgwon <92065911+kimgwon@users.noreply.github.com>
2025-01-28 11:13:00 +0900

    Create 자동차_대여_기록에서_대여중_구분하기.sql

---
+++ Programmers/SQL_고득점_Kit/GROUP_BY/자동차_대여_기록에서_대여중_구분하기.sql
@@ -0,0 +1,26 @@
+-- 코드를 입력하세요
+SELECT CAR_ID,
+    CASE
+        WHEN CAR_ID IN 
+            (
+                SELECT CAR_ID
+                FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                WHERE START_DATE <= '2022-10-16' AND END_DATE >= '2022-10-16'
+            ) 
+            THEN '대여중'
+        ELSE '대여 가능'
+    END AS AVAILABILITY
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+ORDER BY CAR_ID DESC;
+
+-- MAX를 이용할 수 있다.
+-- SELECT CAR_ID,
+--        CASE 
+--            WHEN MAX(CASE WHEN '2022-10-16' BETWEEN START_DATE AND END_DATE THEN 1 ELSE 0 END) = 1 
+--            THEN '대여중' 
+--            ELSE '대여 가능' 
+--        END AS AVAILABILITY
+-- FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+-- GROUP BY CAR_ID
+-- ORDER BY CAR_ID DESC;
